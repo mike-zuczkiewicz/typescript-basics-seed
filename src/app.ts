6a5bcd625f0643e4cc7f10f8bd45caa7dc784cea
Mike Z <okipapa@me.com>
2025-02-10 07:38:35 +0100

    vs code config update

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('Ahoy TypeScript!');
+const pizzaCost: number = 10;
+const pizzaToppings: number = 2;
+
+function calculatePrice(cost: number, toppings: number): number {
+  return cost + 1.5 * toppings;
+}
+
+const cost: number = calculatePrice(pizzaCost, pizzaToppings);
+console.log(`Pizza costs: ${cost}`);
