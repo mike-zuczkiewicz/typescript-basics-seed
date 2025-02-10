9d5837b68cb42117bdedc43990054d690e9cc9c7
Mike Z <okipapa@me.com>
2025-02-10 07:55:42 +0100

    basic types

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-const pizzaCost: number = 10;
-const pizzaToppings: number = 2;
+const coupon: string = 'pizza25';
 
-function calculatePrice(cost: number, toppings: number): number {
-  return cost + 1.5 * toppings;
+function normalizeCoupon(code: string): string {
+    return code.toUpperCase();
 }
 
-const cost: number = calculatePrice(pizzaCost, pizzaToppings);
-console.log(`Pizza costs: ${cost}`);
+const couponMessage: string = `Final coupon is ${normalizeCoupon(coupon)}`;
+
+console.log(couponMessage);
